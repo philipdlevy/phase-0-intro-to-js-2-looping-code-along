653f65c7a1f0b63f1bca08250f906df23fc0f31c
Philip Levy <philipdlevy@gmail.com>
2022-02-17 11:09:33 -0700

    commit tree d5ab0fff85df36d09884b2d24955eabff9b00e1e

--- index.js
+++ index.js
@@ -1,43 +1,14 @@
-// Code your solutions in this file
-/*
-const gifts = ["teddy bear", "drone", "doll"];
-
-function wrapGifts(gifts) {
-  for (let i = 0; i < gifts.length; i++) {
-    console.log(`Wrapped ${gifts[i]} and added a bow!`);
-  };
-
-  return gifts;
-}
-
-wrapGifts(gifts);
-*/
-
-
-function writeCards(names, birthday) {
-    const thankYou = [];
-    for (let i = 0; i < names.length; i++) {
-        thankYou.push(`Thank you, ${names[i]}, for the wonderful ${birthday} gift!` );
-    }
-    return thankYou;
+function writeCards (names, birthday) {
+  const customMessage = [];
+  for (let i = 0; i < names.length; i++) {
+     customMessage.push(`Thank you, ${names[i]}, for the wonderful ${birthday} gift!`)
+  }
+  return customMessage;
 };
 
-
-
-/*
 function countDown() {
-    let x = 0;
-while (x < 11) {
-  console.log(x); 
-  x++;
-}
-}
-*/
-
-function countDown() {
-    let x = 10;
-while (x >= 0) {
-  console.log(x); 
-  x--;
-}
-}
+  let i = 10;
+  while (i >= 0) {
+    console.log (i--)
+  }
+};
